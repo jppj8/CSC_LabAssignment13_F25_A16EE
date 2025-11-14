370d4b99ca53696b9bd32d29d9981a584ee4b13f
jppj8 <jtejada@broncos.uncfsu.edu>
2025-11-14 11:24:26 -0500

    Implement initialization method with attributes
    Added an initialization method with two instance attributes: 'plan' and 'size'.

--- class_3_initialization.py
+++ class_3_initialization.py
@@ -3,6 +3,12 @@ class House:
     middle_school = "FV Middle"
     high_school = "FV High"
 
+    # initialization method with two instance attributes
+    def __init__(self):
+        self.plan = "Eden"   # instance attribute
+        self.size = 2000     # instance attribute
+
+
     # TODO: create an initialization method, and two instance attributes:
     # attribute name "plan" with string value "Eden"
     # attribute name "size" with int value 2000
