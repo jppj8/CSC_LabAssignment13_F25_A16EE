0b5486187815788b64e10b9a9a2c34348f9a88cf
Tieming <yopming@users.noreply.github.com>
2025-11-14 10:14:20 -0500

    Update class_4_instance_attr.py

--- class_4_instance_attr.py
+++ class_4_instance_attr.py
@@ -4,6 +4,6 @@ class House:
     high_school = "FV High"
 
     # TODO: create an initialization method, and three instance attributes:
-    # attribute name "size" with int value 2000
+    # attribute name "size" with int value 2500
     # attribute name "owner" from argument during class calling
     # attribute name "address" from argument during class calling
