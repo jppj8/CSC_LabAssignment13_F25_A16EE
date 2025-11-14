0d4b67206bc0c33f567082d552df05e34372ab12
jppj8 <jtejada@broncos.uncfsu.edu>
2025-11-14 11:22:42 -0500

    Add class attributes to House class
    Defined three class attributes for the House class.

--- class_2_class_attr.py
+++ class_2_class_attr.py
@@ -1,4 +1,9 @@
 class House:
+    # class attributes
+    city = "Cary"
+    community = "Palm River"
+    builder = "Python"
+
     # TODO: define three class attributes:
     # attribute name "city" with string value "Cary"
     # attribute name "community" with string value "Palm River"
